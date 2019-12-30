b203270e592888597b2c0845328b7473407fb2a4
Chow Jia Ying <chowjiaying211@gmail.com>
2019-12-30 20:44:35 +0800

    Adds a new 'List' model

---
+++ db/migrate/20191230124018_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :lists do |t|
+      t.string :key
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
