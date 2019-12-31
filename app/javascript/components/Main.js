12db2fee1c02a3d0caa1c660e660d3b5dec36b1b
Chow Jia Ying <chowjiaying211@gmail.com>
2019-12-31 12:35:42 +0800

    Adds a Main Component [React]

---
+++ app/javascript/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AllFruits from "./AllFruits"
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Fruits are great!</h1>
+                <AllFruits />
+            </div>
+        );
+    }
+}
+
+export default Main
