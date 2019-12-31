e6814aa9527e81eb966307bcf3ccdd89577ab5cd
Chow Jia Ying <chowjiaying211@gmail.com>
2019-12-31 16:15:13 +0800

    Adds methods to send POST requests
    This allows users to create new fruits

--- app/javascript/components/Body.js
+++ app/javascript/components/Body.js
@@ -1,11 +1,39 @@
 import React from "react";
 import AllFruits from "./AllFruits"
+import NewFruit from "./NewFruit";
 class Body extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             fruits: []
         };
+
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.addNewFruit = this.addNewFruit.bind(this);
+    }
+
+    handleFormSubmit(name, description) {
+        let body = JSON.stringify({fruit: {name: name, description: description}});
+        
+        fetch('http://localhost:3000/api/v1/fruits', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            
+            body: body,
+        }).then((response) => {
+            return response.json();
+        })
+        .then((fruit) => {
+            this.addNewFruit(fruit)
+        });
+    }
+
+    addNewFruit(fruit) {
+        this.setState({
+            fruits: this.state.fruits.concat(fruit)
+        })
     }
 
     componentDidMount() {
@@ -17,6 +45,7 @@ class Body extends React.Component {
     render() {
         return(
             <div>
+                <NewFruit handleFormSubmit={this.handleFormSubmit}/>
                 <AllFruits fruits={this.state.fruits} />
             </div>
         )
