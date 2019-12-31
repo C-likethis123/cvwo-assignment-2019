7639f8bb1530b2e5893a8e11effab2e20e0b3104
Chow Jia Ying <chowjiaying211@gmail.com>
2019-12-31 16:15:13 +0800

    Adds methods to send POST requests
    This allows users to create new fruits.
    In the process I encountered a
    'Unexpected token in json at position 0' error. This explains the error:
    https://daveceddia.com/unexpected-token-in-json-at-position-0/
    
    I fixed the bug by adding a line in the Application controller.

--- app/javascript/components/NewFruit.js
+++ app/javascript/components/NewFruit.js
@@ -3,8 +3,10 @@ import React from "react";
 class NewFruit extends React.Component {
   render() {
     let formFields = {};
+    let handleFormSubmit = this.props.handleFormSubmit;
     return (
-      <form>
+      <form onSubmit={ (e) => { e.preventDefault(); handleFormSubmit(formFields.name.value, formFields.description.value); e.target.reset(); }
+    } >
         <input
           ref={input => (formFields.name = input)}
           placeholder="Enter the name of the fruit"
