cca151eb7ad7a3bce1b6e94fa4397c8a00a0a2a7
Chow Jia Ying <chowjiaying211@gmail.com>
2020-01-03 21:55:10 +0800

    Allow List component to retrieve tasks from API

--- app/javascript/components/List.js
+++ app/javascript/components/List.js
@@ -3,20 +3,30 @@ class List extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: ['HELLO', 'BYE']
+      tasks: []
     };
   }
 
+  componentDidMount() {
+    fetch(`/api/v1/lists/${this.props.id}/tasks.json`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({ tasks: data });
+      });
+  }
+
   render() {
-    let tasks = this.state.tasks.map(task => {
-      return <div>{task}</div>;
-    });
+    let tasks = [];
+    this.state.tasks.forEach(task =>
+      tasks.push(<div key={task.id}>{task.title}</div>)
+    );
     return (
       <div className="todo-list" key={this.props.id}>
         <div className="todo-list-title">{this.props.title}</div>
-        <div className="items-container">
-            {tasks}
-        </div>
+        <div className="items-container">{tasks}</div>
       </div>
     );
   }
