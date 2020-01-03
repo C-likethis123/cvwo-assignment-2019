c1916c082caf78b5990de6ae702c5ba184ff2438
Chow Jia Ying <chowjiaying211@gmail.com>
2020-01-03 21:57:58 +0800

    Adds a column to the Task model
    The column contains a boolean value isDailies, to differentiate
    between dailies and one-off tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_124018) do
+ActiveRecord::Schema.define(version: 2020_01_03_133255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,5 +45,19 @@ ActiveRecord::Schema.define(version: 2019_12_30_124018) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "deadline"
+    t.boolean "isCompleted"
+    t.string "tags"
+    t.bigint "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "isDailies"
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+  end
+
   add_foreign_key "comments", "articles"
+  add_foreign_key "tasks", "lists"
 end
