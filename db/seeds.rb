7da12a0989d18b4a2c70e21135df2fe321c9ad28
Chow Jia Ying <chowjiaying211@gmail.com>
2020-01-03 21:56:31 +0800

    Updates seeds.rb with code to add tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-list = List.create([{title: 'Dailies', key: '1'}])
+# Creating lists
+# lists = [['Dailies', 1], ['One off tasks', 2]] 
+# lists.each{|list| List.create({title: list[0], key: "#{list[1]}"})}
+
+# Creating To Dos
+# lists = List.all
+# for i in 0...2
+#     for j in 0...5
+#         lists[i].tasks.create(
+#             {title: "Do something #{j}",
+#             description: "Description #{j}",
+#             deadline: DateTime.now,
+#             isCompleted: false,
+#             isDailies: lists[i].title == 'Dailies',
+#             tags: "do now, something else"})
+#     end
+# end
