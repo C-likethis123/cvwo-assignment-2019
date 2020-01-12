c7979875e6482ba51a156d012500ded72d7acd26
Chow Jia Ying <chowjiaying211@gmail.com>
2020-01-12 21:04:21 +0800

    Conditionally render Task Modals
    Previously, all Task Modals will be rendered, but not displayed.
    Now, the Task Modals will only be rendered when editing a task.

--- app/javascript/components/Task.js
+++ app/javascript/components/Task.js
@@ -52,37 +52,45 @@ class Task extends React.Component {
         <Checkbox onClick={this.handleClick} />
         <div className="content-display">
           <div>{this.props.task.title}</div>
-          <div className="other-info">Description: {this.props.task.description}</div>
-          <div className="other-info">Deadline: {this.displayDate(this.props.task.deadline)}</div>
+          <div className="other-info">
+            Description: {this.props.task.description}
+          </div>
+          <div className="other-info">
+            Deadline: {this.displayDate(this.props.task.deadline)}
+          </div>
           <div className="other-info">Tags: {this.props.task.tags}</div>
         </div>
 
         <Button
           icon
           size="mini"
+          attached="right"
           onClick={() => this.setState({ isModalOpen: true })}
         >
           <Icon name="edit"></Icon>
         </Button>
         <Button
           icon
+          attached="right"
           size="mini"
           color="red"
           onClick={() => this.props.handleDelete(this.state.task)}
         >
           <Icon name="trash"></Icon>
         </Button>
-        <TaskModal
-          title={this.state.task.title}
-          description={this.state.task.description}
-          deadline={this.state.task.deadline}
-          description={this.state.task.description}
-          isEditable={true}
-          isModalOpen={this.state.isModalOpen}
-          handleAdd={this.props.handleAdd}
-          updateTask={this.updateTask}
-          handleClose={() => this.setState({ isModalOpen: false })}
-        />
+        {this.state.isModalOpen ? (
+          <TaskModal
+            title={this.state.task.title}
+            description={this.state.task.description}
+            deadline={this.state.task.deadline}
+            description={this.state.task.description}
+            isEditable={true}
+            isModalOpen={this.state.isModalOpen}
+            handleAdd={this.props.handleAdd}
+            updateTask={this.updateTask}
+            handleClose={() => this.setState({ isModalOpen: false })}
+          />
+        ) : null}
       </div>
     );
   }
