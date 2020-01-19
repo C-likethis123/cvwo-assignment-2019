4fcab8102e2783cf12775a7a2820fbac97634165
Chow Jia Ying <chowjiaying211@gmail.com>
2020-01-19 11:05:51 +0800

    Add users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_11_172103) do
+ActiveRecord::Schema.define(version: 2020_01_19_023815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,5 +35,15 @@ ActiveRecord::Schema.define(version: 2020_01_11_172103) do
     t.index ["list_id"], name: "index_tasks_on_list_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.bigint "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_users_on_list_id"
+  end
+
   add_foreign_key "tasks", "lists"
+  add_foreign_key "users", "lists"
 end
