2ddc706cfa53cd6cf3704ad0acbbcf86f4dde959
Chow Jia Ying <chowjiaying211@gmail.com>
2020-01-19 15:37:22 +0800

    Add functionality to SignUp form inputs

--- app/javascript/components/SignUp.js
+++ app/javascript/components/SignUp.js
@@ -4,7 +4,35 @@ import { Segment, Form, Button } from "semantic-ui-react";
 class SignUp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleChange = event => {
+    const attribute = event.target.id;
+    const changedValue = event.target.value;
+    this.setState(() => {
+      return {
+        [attribute]: changedValue
+      };
+    });
+  };
+
+  handleSignUp = () => {
+      const newUser = Object.assign({}, this.state);
+      console.log(newUser);
+      fetch(`/api/v1/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ user: newUser}),
+      })
+        .then(response => {
+            console.log(response.json());
+        });
   }
 
   render() {
@@ -14,20 +42,24 @@ class SignUp extends React.Component {
           <h1>Sign Up</h1>
           <Form>
             <Form.Field>
-              <label>Username or Email</label>
-              <input placeholder="Enter your username or email"></input>
+              <label>Email</label>
+              <input placeholder="Enter your email" type="email" id="email" onChange={this.handleChange}></input>
             </Form.Field>
 
             <Form.Field>
               <label>Password</label>
-              <input placeholder="Enter a password"></input>
+              <input placeholder="Enter a password" type="password" id="password" onChange={this.handleChange}></input>
             </Form.Field>
 
             <Form.Field>
               <label>Confirm Password</label>
-              <input placeholder="Enter your password again"></input>
+              <input
+                placeholder="Enter your password again"
+                id="password-confirmation"
+                type="password"
+              ></input>
             </Form.Field>
-            <Button type="submit">Sign Up</Button>
+            <Button onClick={this.handleSignUp}>Sign Up</Button>
           </Form>
         </div>
       </div>
