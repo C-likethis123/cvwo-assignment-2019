fd98763edde7673ab315dba8a29d5b134ef9ca7d
Chow Jia Ying <chowjiaying211@gmail.com>
2020-01-20 23:43:25 +0800

    Remove lists association with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :lists
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  # has_many :lists
 end
