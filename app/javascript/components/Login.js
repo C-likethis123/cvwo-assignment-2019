9cd078426d74566dda070a6bf2d115e3daa56f81
Chow Jia Ying <chowjiaying211@gmail.com>
2020-01-21 13:34:05 +0800

    Add functionality to handle log ins

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -1,28 +1,81 @@
 import React from "react";
-import { Segment, Form, Button } from "semantic-ui-react";
+import { Message, Form, Button } from "semantic-ui-react";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      password: "",
+      hasError: false,
+    };
   }
 
+  handleChange = event => {
+    const attribute = event.target.id;
+    const changedValue = event.target.value;
+    this.setState(() => {
+      return {
+        [attribute]: changedValue
+      };
+    });
+  };
+
+  handleLogIn = () => {
+    const userCredentials = Object.assign({}, this.state);
+    fetch(`users/sign_in`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      redirect: "follow",
+      body: JSON.stringify({ user: userCredentials })
+    }).then(response => {
+      if (response.redirected) {
+        window.location = response.url;
+      } else {
+          this.setState(() => ({ hasError: true }));
+      }
+    });
+  };
+
   render() {
+    const errorMessages =
+      this.state.hasError ? (
+        <Message error header="Your username/password is invalid" />
+      ) : null;
     return (
       <div className="container">
         <div className="sign-up">
+          {errorMessages}
           <h1>Sign In</h1>
           <Form>
             <Form.Field>
               <label>Email</label>
-              <input type="email" placeholder="Enter your email"></input>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                onChange={this.handleChange}
+              ></input>
             </Form.Field>
 
             <Form.Field>
               <label>Password</label>
-              <input type="password" placeholder="Enter a password"></input>
+              <input
+                id="password"
+                type="password"
+                placeholder="Enter a password"
+                onChange={this.handleChange}
+              ></input>
             </Form.Field>
-            <Button type="submit">Sign In</Button>
+            <Button
+              type="submit"
+              onClick={this.handleLogIn}
+              onChange={this.handleChange}
+            >
+              Sign In
+            </Button>
           </Form>
         </div>
       </div>
