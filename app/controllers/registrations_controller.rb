6a9eb1b67e5ad45f65e788d76ac10174aaa1a49c
Chow Jia Ying <chowjiaying211@gmail.com>
2020-01-21 12:22:32 +0800

    Change users_controller to registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::UsersController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
     def create
         @user = User.new(user_params)
         if @user.save
@@ -9,6 +9,6 @@ class Api::V1::UsersController < ApplicationController
     end
 
     private def user_params
-        params.require(:user).permit(:id, :email, :password)
+        params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
