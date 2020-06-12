644cfa098c7cabf2e14a1f4840a4cd14757a1fb1
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-12 22:33:08 +0800

    Refactor: AllLists to a functional component

--- app/javascript/components/AllLists.js
+++ app/javascript/components/AllLists.js
@@ -1,36 +1,16 @@
 import React from "react";
 import List from "./List";
 
-class AllLists extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lists: [],
-      tagOptions: [],
-    };
-  }
-
-  render() {
-    let lists = this.props.lists.map(list => {
-      return (
-        <List
-          key={list.id}
-          id={list.id}
-          title={list.title}
-          updateTagOptions={this.props.updateTagOptions}
-          searchKeywords={this.props.searchKeywords}
-          searchTags={this.props.searchTags}
-          viewCompleted={this.props.viewCompleted}
-        />
-      );
-    });
-
-    return (
-      <div>
-        <div className="list-display">{lists}</div>
-      </div>
-    );
-  }
-}
+const AllLists = (props) => {
+  return (
+    <div className="list-display">
+      {props.lists.map((list) => {
+        return (
+          <List key={list.id} id={list.id} title={list.title} {...props} />
+        );
+      })}
+    </div>
+  );
+};
 
 export default AllLists;
