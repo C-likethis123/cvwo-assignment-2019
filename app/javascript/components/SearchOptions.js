311d685b4568ff5f70b81e880dfb4bcb8b23c857
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-12 22:20:55 +0800

    Refactor: SearchOptions to a functional component

--- app/javascript/components/SearchOptions.js
+++ app/javascript/components/SearchOptions.js
@@ -1,37 +1,29 @@
 import React from "react";
 import { Input, Dropdown, Radio } from "semantic-ui-react";
 
-class SearchOptions extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="search-options">
-        <Input
-          icon="search"
-          placeholder="Search for tasks..."
-          onChange={(event, data) =>
-            this.props.updateSearchKeywords(data.value)
-          }
-        />
-        <Dropdown
-          placeholder="Filter Tasks"
-          multiple
-          search
-          selection
-          options={this.props.tagOptions}
-          onChange={(event, data) => this.props.updateSearchTags(data.value)}
-        />
-        <Radio
-          toggle
-          label="View completed tasks"
-          onChange={this.props.toggleViewCompleted}
-        />
-      </div>
-    );
-  }
-}
+const SearchOptions = (props) => {
+  return (
+    <div className="search-options">
+      <Input
+        icon="search"
+        placeholder="Search for tasks..."
+        onChange={(event, data) => props.updateSearchKeywords(data.value)}
+      />
+      <Dropdown
+        placeholder="Filter Tasks"
+        multiple
+        search
+        selection
+        options={props.tagOptions}
+        onChange={(event, data) => props.updateSearchTags(data.value)}
+      />
+      <Radio
+        toggle
+        label="View completed tasks"
+        onChange={props.toggleViewCompleted}
+      />
+    </div>
+  );
+};
 
 export default SearchOptions;
