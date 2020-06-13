4b647c448da956a04b33428eb15c944f9925d3b4
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-13 18:57:43 +0800

    Misc: Add string constants of redux actions

---
+++ app/javascript/action-types.js
@@ -0,0 +1,6 @@
+export const ADD_TODO = "ADD_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const LOAD_TODO = "LOAD_TODO";
+export const TAGS_FILTER = "TAGS_FILTER";
+export const KEYWORDS_FILTER = "KEYWORDS_FILTER";
