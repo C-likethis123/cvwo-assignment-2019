471d53616b23dbd3c72bcc6d712a28bd0742230d
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-13 21:08:51 +0800

    Add a DailyList component
    The DailyList component is composed of a List component but stores Daily specific tasks.
    Distinguishing two different lists makes it easier for each list to be subscribed to a different list array.

---
+++ app/javascript/components/DailyList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import List from "./List";
+import { addToDo } from "../actions";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => {
+  const [dailyList] = state.lists.filter(
+    (list) => list.title === "Daily Tasks"
+  );
+  return { tasks: state.dailyTasks, ...dailyList };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addTask: (todo) => dispatch(addToDo(todo, true)),
+});
+const ConnectedDailyList = (props) => {
+  return <List isDailies={true} title="Daily Tasks" {...props} />;
+};
+
+const DailyList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ConnectedDailyList);
+export default DailyList;
