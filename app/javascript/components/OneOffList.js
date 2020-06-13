78048c0f31f843901fcda7c4afce9dc970709ad1
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-13 21:13:10 +0800

    Add a OneOffList component
        The OneOffList component is composed of a List component but stores one-off tasks.
        Distinguishing two different lists makes it easier for each list to be subscribed to a different list array.

---
+++ app/javascript/components/OneOffList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import List from "./List";
+import { addToDo } from "../actions";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => {
+  const [oneOffList] = state.lists.filter(
+    (list) => list.title !== "Daily Tasks"
+  );
+  return { tasks: state.oneOffTasks, ...oneOffList };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addTask: (todo) => dispatch(addToDo(todo, false)),
+});
+const ConnectedOneOffList = (props) => {
+  return <List isDailies={true} title="Daily Tasks" {...props} />;
+};
+
+const OneOffList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ConnectedOneOffList);
+export default OneOffList;
