506852726d73cbaeb90623736e418dadca1a8231
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-14 21:23:08 +0800

    Refactor: Remove searchKeywords from List.js

--- app/javascript/components/List.js
+++ app/javascript/components/List.js
@@ -67,15 +67,6 @@ class List extends React.Component {
     }));
   };
 
-  matchesSearchKeywords = (task, searchKeywords) => {
-    const taskTitle = task.title.toLowerCase();
-    const taskDescription = task.description.toLowerCase();
-    return (
-      taskTitle.includes(searchKeywords) ||
-      taskDescription.includes(searchKeywords)
-    );
-  };
-
   matchesSearchTags = (task, searchTags) => {
     const taskTags = task.tags.split(", ");
     const matchesSearchTags =
