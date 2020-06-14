b4499e6fbfb4162f3ac1540c1d9c372dd402f63d
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-14 21:11:59 +0800

    Refactor: Daily and OneOffList to ConnectedList
    Previously DailyList and OneOffList were two different list components.
    But they were very similar and introducing changes in list components meant
    I had to duplicate a lot of code written and increased the possibilities of errors.
    
    The solution is to create a generic component, ConnectedList, which subscribes
    to the Redux store.

--- app/javascript/components/ConnectedList.js
+++ app/javascript/components/ConnectedList.js
@@ -3,8 +3,6 @@ import List from "./List";
 import { addToDo, updateToDo, deleteToDo, loadToDo } from "../actions";
 import { connect } from "react-redux";
 
-const isDailies = true;
-
 const matchesSearchKeywords = (task, searchKeywords) => {
   const taskTitle = task.title.toLowerCase();
   const taskDescription = task.description.toLowerCase();
@@ -13,6 +11,7 @@ const matchesSearchKeywords = (task, searchKeywords) => {
     taskDescription.includes(searchKeywords)
   );
 };
+
 const getVisibleTasks = (tasks, isCompleted, searchKeywords) => {
   const visibleTasks = tasks
     .filter((task) => task.isCompleted === isCompleted)
@@ -20,32 +19,31 @@ const getVisibleTasks = (tasks, isCompleted, searchKeywords) => {
 
   return visibleTasks;
 };
-const mapStateToProps = (state) => {
-  const [dailyList] = state.lists.filter(
-    (list) => list.title === "Daily Tasks"
-  );
+
+const mapStateToProps = (state, { isDailies }) => {
+  const taskList = isDailies ? state.dailyTasks : state.oneOffTasks;
+  const listInformation = isDailies ? state.lists[0] : state.lists[1];
   return {
-    tasks: getVisibleTasks(
-      state.dailyTasks,
-      state.viewCompleted,
-      state.searchKeywords
-    ),
-    ...dailyList,
+    tasks: getVisibleTasks(taskList, state.viewCompleted, state.searchKeywords),
+    ...listInformation,
   };
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  addTask: (todo) => dispatch(addToDo(todo, isDailies)),
-  updateTask: (todo) => dispatch(updateToDo(todo, isDailies)),
-  deleteTask: (todo) => dispatch(deleteToDo(todo, isDailies)),
-  loadTasks: (listId) => dispatch(loadToDo(listId, isDailies)),
-});
-const ConnectedDailyList = (props) => {
-  return <List isDailies={true} title="Daily Tasks" {...props} />;
+const mapDispatchToProps = (dispatch, { isDailies }) => {
+  return {
+    addTask: (todo) => dispatch(addToDo(todo, isDailies)),
+    updateTask: (todo) => dispatch(updateToDo(todo, isDailies)),
+    deleteTask: (todo) => dispatch(deleteToDo(todo, isDailies)),
+    loadTasks: (listId) => dispatch(loadToDo(listId, isDailies)),
+  };
 };
 
-const DailyList = connect(
+const ConnectedDailyList = (props) => {
+  return <List isDailies={props.isDailies} title={props.title} {...props} />;
+};
+
+const ConnectedList = connect(
   mapStateToProps,
   mapDispatchToProps
 )(ConnectedDailyList);
-export default DailyList;
+export default ConnectedList;
