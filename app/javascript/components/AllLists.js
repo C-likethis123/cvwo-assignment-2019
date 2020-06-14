733277e6d33ff967ddfeb75d6055d4aef3bdf54a
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-14 21:19:10 +0800

    Refactor: Use generic ConnectedList in AllLists

--- app/javascript/components/AllLists.js
+++ app/javascript/components/AllLists.js
@@ -1,17 +1,18 @@
 import React from "react";
-import DailyList from "./DailyList";
-import OneOffList from "./OneOffList";
+import ConnectedList from "./ConnectedList";
 import { connect } from "react-redux";
 
+const isDailies = (list) => list.title === "Daily Tasks";
 const mapStateToProps = (state) => {
   return { lists: state.lists };
 };
 
-const AllLists = () => {
+const AllLists = ({ lists }) => {
   return (
     <div className="list-display">
-      <DailyList />
-      <OneOffList />
+      {lists.map((list) => (
+        <ConnectedList key={list.id} isDailies={isDailies(list)} />
+      ))}
     </div>
   );
 };
