bc92f9c18018f8caea755622d24d66052afd79d0
Chow Jia Ying <chowjiaying211@gmail.com>
2020-06-14 22:45:17 +0800

    Refactor: Remove code related to tags in Main.js
    Filter tags logic have been moved to Redux actions and reducers.

--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -18,51 +18,15 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTags: [],
-      tagOptions: [],
       lists: [],
     };
   }
-
-  updateSearchTags = (searchTags) => {
-    this.setState((prevProps, prevState) => {
-      return { searchTags: searchTags };
-    });
-  };
 
   processTags = (tags) => {
     return tags
       .split(", ")
       .map((tag) => tag.trim())
       .filter((tag) => tag !== "");
-  };
-
-  updateTagOptions = () => {
-    let tagOptions = [];
-    this.state.lists.forEach((list) => {
-      fetch(`/api/v1/lists/${list.id}/tasks.json`)
-        .then((response) => response.json())
-        .then((data) => {
-          data.forEach((task) => {
-            let tagsFromTasks = this.processTags(task.tags);
-            tagOptions = tagOptions.concat(tagsFromTasks);
-          });
-          return tagOptions;
-        })
-        .then((tagOptions) => {
-          tagOptions = [...new Set(tagOptions)];
-          tagOptions = tagOptions.map((tag) => ({
-            key: tag,
-            value: tag,
-            text: tag,
-            label: { color: "red", empty: true, circular: true },
-          }));
-
-          this.setState(() => ({
-            tagOptions: tagOptions,
-          }));
-        });
-    });
   };
 
   render() {
