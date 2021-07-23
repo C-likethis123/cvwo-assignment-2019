5f6fe0b988aabefd84da75055a4d572fdb4f878e
Chow Jia Ying <chowjiaying211@gmail.com>
2021-07-23 21:22:21 +0800

    feat: update task API to complete tasks in one SQL request

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -5,27 +5,24 @@ class Api::V1::TasksController < ApplicationController
     end
 
     def create
-        @list = List.find(params[:list_id])
-        @task = @list.tasks.create(task_params)
+        @task = Task.create(task_params)
         render json: @task
     end
 
     def update
-        @list = List.find(params[:list_id])
-        @task = @list.tasks.find(params[:id])
+        @task = Task.find(params[:id])
         if @task.update(task_params)
             render json: @task
         end
     end
 
     def destroy
-        @list = List.find(params[:list_id])
-        @task = @list.tasks.find(params[:id])
+        @task = Task.find(params[:id])
         @task.destroy
     end
     
     private def task_params
-        params.require(:task).permit(:title, :description, :deadline, :tags,
+        params.permit(:title, :description, :deadline, :tags,
         :id, :isCompleted, :list_id, :created_at, :updated_at, :isDailies)
     end
 end
