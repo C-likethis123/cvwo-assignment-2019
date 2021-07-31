c17187925ea9795e0b57f845f639c0a69d629824
Chow Jia Ying <chowjiaying211@gmail.com>
2021-08-01 01:47:02 +0800

    feat: add webpack plugins to optimise compilation times

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+environment.splitChunks();
+const config = environment.toWebpackConfig();
 
-module.exports = environment.toWebpackConfig()
+module.exports = config
